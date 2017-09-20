364e4d31e3bf6fc035457cb3730cc27765f9a210
Abhishek Singh Thakur <mailabhi593@gmail.com>
2017-09-20 13:55:26 +0000

    Add files via upload

---
+++ CPTTRN5/ptrn5.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+
+
+using namespace std;
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+    int l, c, s;
+    scanf("%d %d %d", &l, &c, &s);
+    const int breadth = (c * (s + 1)) + 1;
+    const int height = (l * (s + 1)) + 1;
+    for(int i = 0; i < height; i++)
+    {
+      for(int j = 0; j < breadth; j++)
+      {
+        if(i % (s+1) == 0 || j % (s+1) == 0)
+        {
+          printf("*");
+        }
+        else if((i/(s+1) + j/(s+1)) % 2 == 0)
+        {
+          if(i % (s+1) == j % (s+1))
+          {
+            printf("\\");
+          }
+          else
+          {
+             printf(".");
+          }
+        }
+        else
+        {
+          if(i % (s+1) == (s+1) - (j % (s+1)))
+          {
+            printf("/");
+          }
+          else
+          {
+            printf(".");
+          }
+        }
+      }
+      printf("\n");
+    }
+    printf("\n");
+   }
+}

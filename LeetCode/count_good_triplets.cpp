aa0050425176f2118298ba211b3908634719e0da
Abhishek Singh Thakur <mailabhi593@gmail.com>
2021-11-17 18:19:51 +0530

    Create count_good_triplets.cpp

---
+++ LeetCode/count_good_triplets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int cnt = 0;
+        
+        for (int i = 0; i < arr.size()-2; ++i)
+        {
+            for (int j = i+1; j < arr.size()-1; ++j)
+            {
+                for (int k = j+1; k < arr.size(); ++k)
+                {
+                    if (abs(arr[i] - arr[j]) <= a &&
+                       abs(arr[k] - arr[j]) <= b && 
+                       abs(arr[i] - arr[k]) <= c)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};

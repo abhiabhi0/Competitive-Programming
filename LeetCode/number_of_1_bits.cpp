41d4ce3494acfc21f583dbed780bdc7764efe559
Abhishek Singh Thakur <mailabhi593@gmail.com>
2021-11-24 09:19:09 +0530

    Create number_of_1_bits.cpp

---
+++ LeetCode/number_of_1_bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        while (n)
+        {
+            if (n%2 != 0)
+            {
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+};

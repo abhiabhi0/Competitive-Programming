9f380c37649d8141921a50d7b212805a2b67fb89
Abhishek Singh Thakur <mailabhi593@gmail.com>
2022-05-29 10:38:39 +0530

    Create DP_lcs.cpp

---
+++ LeetCode/DP_lcs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        
+        vector< vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        
+        for (int i = 1; i <= n1; ++i)
+        {
+            for (int j = 1; j <= n2; ++j)
+            {
+                if (text1[i-1] == text2[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else 
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};
